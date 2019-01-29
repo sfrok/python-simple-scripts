7e990100dd586201a410640608ad94c575a1116a
Sergey <sfrok99@gmail.com>
2019-01-29 18:42:09 +0200

    First easy task, rectangle area

---
+++ tasks/easy/rectangle area.py
@@ -0,0 +1,9 @@
+# Task: Write the program which is count rectangle area.
+
+
+height = int(input('Enter the height: '))
+width = int(input('Enter the width: '))
+
+print('Rectangle area is: ', height * width)
+
+
