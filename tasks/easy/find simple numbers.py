11adc538503fe0eb71c3e3d5a4eaf33c0b9463c8
Sergey <sfrok99@gmail.com>
2019-01-30 21:56:53 +0200

    Task for search simple numbers in range

---
+++ tasks/easy/find simple numbers.py
@@ -0,0 +1,16 @@
+# Task: Create program which find simple numbers between 0 and customer number.
+
+
+customer_number = int(input('Enter natural number: '))
+simple_numbers = []
+
+for number in range(0, customer_number):
+    if number > 1:
+        for divider in range(2, number):
+            if number % divider == 0:
+                break
+        else:
+            simple_numbers.append(number)
+
+
+print('Simple numbers: ', simple_numbers)
