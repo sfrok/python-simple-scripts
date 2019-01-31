24e2f5f68f332febe125daa2701cf73da80114b5
Sergey <sfrok99@gmail.com>
2019-01-31 11:26:42 +0200

    Task on multiples numbers, test base operations

---
+++ tasks/easy/multiples numbers.py
@@ -0,0 +1,13 @@
+# Task: Create program which print all numbers multiples 5, between in two customer numbers.
+
+
+first_num = int(input('Enter firs number: '))
+second_num = int(input('Enter second number: '))
+numbers = []
+
+for number in range(first_num, second_num):
+    if number % 5 == 0:
+        numbers.append(number)
+
+
+print(numbers)
