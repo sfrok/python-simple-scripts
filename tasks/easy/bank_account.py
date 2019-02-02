3c47bee36e5c886e08973401969b21a2baae69b2
Sergey <sfrok99@gmail.com>
2019-02-02 22:47:39 +0200

    test function, add options 2

--- tasks/easy/bank_account.py
+++ tasks/easy/bank_account.py
@@ -1,3 +1,5 @@
+# option 1
+
 def bank(a, years):
     while years != 0:
         interim_amount = a * 0.1
@@ -10,3 +12,16 @@ a = int(input('Enter deposit amount: '))
 years = int(input('Enter a term: '))
 
 print(bank(a, years))
+
+
+# option2
+def bank(a, years):
+    if years > 0:
+        return bank(a + a / 10, years - 1)
+    print(a)
+
+
+a = int(input('Enter deposit amount: '))
+years = int(input('Enter a term: '))
+
+bank(a, years)
