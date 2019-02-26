ff0464c951b370db483f7e3db547943fe200aecf
Sergey <sfrok99@gmail.com>
2019-02-26 20:02:08 +0200

    fix superfluous lines

--- tasks/simple_task/missing_letter.py
+++ tasks/simple_task/missing_letter.py
@@ -3,13 +3,13 @@
 
 def find_missing_letter(chars):
     new_chars = []
-    alphabet = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    alphabet = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',  
                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
     for smb in chars:
         new_chars.append(smb.upper())
     find_char = new_chars[0]
-    index_find_char = alphabet.index(find_char)  # got index from alphabet
-    cut_main_alphabet = alphabet[index_find_char:index_find_char + 26]  # got cut main alphabet
+    index_find_char = alphabet.index(find_char) # got index from alphabet 
+    cut_main_alphabet = alphabet[index_find_char:] # got cut main alphabet
     for item in cut_main_alphabet:
         if item not in new_chars:
             if smb.islower():
