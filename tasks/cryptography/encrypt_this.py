44def9f405ef2a0c003e68d5f4918e7ba4ce94b6
Sergey <sfrok99@gmail.com>
2019-03-06 17:53:45 +0200

    decided task of cryptography and add test

--- tasks/cryptography/encrypt_this.py
+++ tasks/cryptography/encrypt_this.py
@@ -1,19 +1,26 @@
-'''
-TO BE CONTINUED
-
-import re
-
-
 def encrypt_this(text):
-    list_ascii_synbols = []  # get ascii smb for first letter in the words
-    flag_for_get_first_letter = r"\b\w"
-    get_first_letter = re.findall(flag_for_get_first_letter, text)
-    for symbol in get_first_letter:
-        list_ascii_synbols.append(ord(symbol))
-    return list_ascii_synbols
+    start_text = text.split()
+    change_letter = []  # change place of letters
+    format_words = []  # get mass of crypt each word
+
+    for char in start_text:
+        length_char = len(char)
+        if length_char <= 2:
+            change_letter.append(char)
+        else:
+            cut_first_letter = char[0]
+            cut_second_letter = char[1]
+            cut_some_letter = char[2:length_char - 1]
+            cut_last_letter = char[length_char - 1]
+            change_letter.append(cut_first_letter + cut_last_letter +
+                                 cut_some_letter + cut_second_letter)
+    for word in change_letter:
+        cut_first_letter_change = word[0]
+        cut_other_part = word[1:]
+        format_words.append(str(ord(cut_first_letter_change)) + cut_other_part)
+
+    return ' '.join(format_words)
 
 
-
-print(encrypt_this("A wise old owl lived in an oak"))
-
-'''
+text = input('Enter a string: ')
+print(encrypt_this(text))
--- tests/test_of_encrypt_this.py
+++ tests/test_of_encrypt_this.py
@@ -1,15 +1,11 @@
-'''
 import unittest
 from tasks.cryptography import encrypt_this
 
 
 class Test(unittest.TestCase):
     def for_another_strig(self):
-        self.assertEqual(encrypt_this("65 119esi 111dl 111lw 108dvei 105n 97n 111ka"),
-                         ['6', '1', '1', '1', '1', '1', '9', '1'])
+        self.assertEqual(encrypt_this('Hello'), '72olle')
 
 
 if __name__ == '__main__':
     unittest.main()
-
-'''
